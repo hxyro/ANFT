ded1ffec57571cf094660d8d3ab3dd6e049e310d
hxyro <hxyro0@gmail.com>
2022-06-29 14:42:43 +0530

    api fetching & anft class component

---
+++ client/src/components/anft.js
@@ -0,0 +1,55 @@
+import { Component } from 'react'
+import axios from 'axios'
+
+export class Nft extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    bid() {
+        const { priceref, id, index } = this.props
+        axios
+            .post('http://localhost:8080/bid', {
+                id,
+            })
+            .then(({ data }) => {
+                const { success } = data
+                if (success) {
+                    console.log(priceref.current[index].price)
+                }
+            })
+            .catch((error) => console.log(error))
+    }
+    render() {
+        const { imageSrc, user, priceref, name, index, log } = this.props
+        return (
+            <div className="nftContainer">
+                <div className="box">
+                    <img src={imageSrc} alt="" />
+                </div>
+                <div className="name">
+                    <p>{name}</p>
+                </div>
+                <div className="user">
+                    <p>Owner: {user}</p>
+                </div>
+                <div className="price">
+                    {log ? (
+                        <button
+                            className="cbtn"
+                            onClick={() => {
+                                priceref.current[index].price += 10
+                                this.bid()
+                                this.forceUpdate()
+                            }}
+                        >
+                            bid
+                        </button>
+                    ) : null}
+
+                    <p>{priceref.current[index].price}$</p>
+                </div>
+            </div>
+        )
+    }
+}
